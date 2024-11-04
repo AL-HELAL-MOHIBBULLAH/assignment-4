d08f0433129b076c92b9df661c30c7dbeeedf2a1
Al Helal Mohibbullah <alhelalmohibbullah@gmail.com>
2024-11-04 01:37:39 -0600

    complete feni and quota section

--- js/donate.js
+++ js/donate.js
@@ -16,7 +16,66 @@ document.getElementById("btn-donate-for-noakhali").addEventListener("click", fun
         document.getElementById("total-collection-for-noakhali").innerText = updatedCollection;
         document.getElementById("my-balance").innerText = myNewBalance;
 
+        document.getElementById("donate-amount-for-noakhali").value = ""
 
+    }
+    document.getElementById("donationModal").classList.add("modal-open");
+
+})
+
+// Close Modal Function
+function closeModal() {
+    document.getElementById("donationModal").classList.remove("modal-open");
+}
+
+// donate for Feni flood
+document.getElementById("btn-donate-for-feni").addEventListener("click", function () {
+    const donateAmount = getInputValue("donate-amount-for-feni");
+    const previousCollection = getTextValue("total-collection-for-feni");
+    const myPreviousBalance = getTextValue("my-balance");
+    const myNewBalance = myPreviousBalance - donateAmount;
+    const updatedCollection = donateAmount + previousCollection;
+    const inputFiledLength = document.getElementById("donate-amount-for-feni");
+    const length = inputFiledLength.value.length;
+    if (donateAmount > myPreviousBalance || isNaN(donateAmount) || donateAmount < 0 || length < 0) {
+        alert("Wrong")
+        return
+    }
+
+    else {
+        document.getElementById("total-collection-for-feni").innerText = updatedCollection;
+        document.getElementById("my-balance").innerText = myNewBalance;
+        document.getElementById("donate-amount-for-feni").value = ""
+
+
+    }
+    document.getElementById("donationModal").classList.add("modal-open");
+
+})
+
+// Close Modal Function
+function closeModal() {
+    document.getElementById("donationModal").classList.remove("modal-open");
+}
+
+// Donate For Quota Movement
+document.getElementById("btn-donate-for-quota").addEventListener("click", function () {
+    const donateAmount = getInputValue("donate-amount-for-quota");
+    const previousCollection = getTextValue("total-collection-for-quota");
+    const myPreviousBalance = getTextValue("my-balance");
+    const myNewBalance = myPreviousBalance - donateAmount;
+    const updatedCollection = donateAmount + previousCollection;
+    const inputFiledLength = document.getElementById("donate-amount-for-quota");
+    const length = inputFiledLength.value.length;
+    if (donateAmount > myPreviousBalance || isNaN(donateAmount) || donateAmount < 0 || length < 0) {
+        alert("WronG")
+        return
+    }
+
+    else {
+        document.getElementById("total-collection-for-quota").innerText = updatedCollection;
+        document.getElementById("my-balance").innerText = myNewBalance;
+        document.getElementById("donate-amount-for-quota").value = ""
 
     }
     document.getElementById("donationModal").classList.add("modal-open");
